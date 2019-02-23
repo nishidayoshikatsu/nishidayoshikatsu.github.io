e5dbc69f52c91a9d9c0e941374023613385682a5
nishidayoshikatsu <nsd24technology@gmail.com>
2019-02-24 03:23:10 +0900

    Topページの上部のボタンのエラーを修正

--- function.js
+++ function.js
@@ -1,12 +1,22 @@
 bubbly();
 
 var techbtn = document.getElementById("technical-blog");
-var otbtn = document.getElementById("technical-blog");
+var otbtn = document.getElementById("other-blog");
+var tech = document.getElementById("technical");
+var ot = document.getElementById("other");
 
 techbtn.addEventListener( "click", function() {
     alert("未実装領域だよ！！！！");
 });
 
 otbtn.addEventListener( "click", function() {
+    alert("未実装領域だよ！！！！");
+});
+
+tech.addEventListener( "click", function() {
+    alert("未実装領域だよ！！！！");
+});
+
+ot.addEventListener( "click", function() {
     alert("未実装領域だよ！！！！");
 });
