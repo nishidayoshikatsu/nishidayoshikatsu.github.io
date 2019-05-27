7107c5c80af9afc55af66ee61c269ce7eadba486
nishidayoshikatsu <nsd24technology@gmail.com>
2019-05-27 23:34:45 +0900

    twitterのタイムラインの埋め込みと文章の追加

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,11 @@
 ityped.init(document.querySelector("#ityped"), {
   loop: true,
   showCursor: true,
-  strings: ["Welcome to NSD's Web site！！"]
+  strings: ["Welcome to NSD's Web site！！\n　　つよつよになりたいよ😭😭"]
 })
 
 var techbtn = document.getElementById("blog");
+let backimg;
 
 techbtn.addEventListener( "click", function() {
     alert("未実装領域だよ！！！！ちょっとまって！！なんでもしますから！！！");
@@ -14,7 +15,7 @@ techbtn.addEventListener( "click", function() {
 // with width, height and depth of 50
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  //img = loadImage("../images/textures.png");
+  backimg = loadImage("../images/background2.png");
 }
 
 function draw() {
@@ -25,5 +26,4 @@ function draw() {
   rotateX(frameCount * 0.03);
   rotateY(frameCount * 0.03);
   box(200);
-
 }
